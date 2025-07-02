8bc43614345e074ba2fb2721d1f4fd9a6da8ebad
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-07-02 13:35:09 +0545

    Added the url for render

--- practice3/src/services/Notes.js
+++ practice3/src/services/Notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseurl = 'http://localhost:3001/api/notes'
+const baseurl = `${process.env.REACT_APP_API_URL}/api/notes`;
 
 const getAll = () => {
     const request = axios.get(baseurl)
